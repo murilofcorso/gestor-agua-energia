6cf862fd864b7aebc40efbc187dd424dec8a13a0
Murilo Corso <murilofcorso@gmail.com>
2025-05-07 10:53:00 -0300

    Adicionando contas de água/energia no banco de dados

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
@@ -12,8 +13,9 @@ app.engine('handlebars', exphbs.engine({
 app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
 
-// Configurando arquivos estáticos
+// Configs
 app.use(express.static('public'));
+app.use(express.json());
 
 // Inicia o servidor
 app.listen(PORT, () => {
@@ -24,3 +26,44 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.render('main-page', {});
 });
+
+// Rota adicionar conta de agua
+app.post('/salvar-conta-agua', (req, res) => {
+    const novoDado = req.body;
+
+    let dados = [];
+    if (fs.existsSync('dados-agua.json')) {
+        const conteudo = fs.readFileSync('dados-agua.json', 'utf-8');
+        const json = JSON.parse(conteudo);
+        dados = Array.isArray(json) ? json : [];
+    }
+    dados.push(novoDado);
+    console.log(dados);
+
+
+    fs.writeFileSync('dados-agua.json', JSON.stringify(dados , null, 2));
+    res.json(dados)
+    res.send('Salvo com sucesso!');
+});
+
+// Rota adicionar conta de energia
+app.post('/salvar-conta-energia', (req, res) => {
+    const novoDado = req.body;
+
+    let dados = [];
+    if (fs.existsSync('dados-energia.json')) {
+        const conteudo = fs.readFileSync('dados-energia.json', 'utf-8');
+        const json = JSON.parse(conteudo);
+        dados = Array.isArray(json) ? json : [];
+    }
+    dados.push(novoDado);
+    console.log(dados);
+
+
+    fs.writeFileSync('dados-energia.json', JSON.stringify(dados , null, 2));
+    res.json(dados)
+    res.send('Salvo com sucesso!');
+});
+
+
+
